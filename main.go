5a36d32db467a059ba19689bdc7c9248cef38ab9
plastikov <siruthmaanzakariyya@gmail.com>
2022-12-15 14:52:47 +0100

    refactored to allow handling of json and yaml file added new path to map object created a global variable from flag input

--- main.go
+++ main.go
@@ -1,34 +1,55 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
 	"flag"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"path/filepath"
 
 	"github.com/plastikov/urlshort/handler"
 )
 
 func main() {
-	yamlFlag := flag.String("File", "urls.yaml", "Pass into this flag a file holding data of yaml type")
+	
 	mux := defaultMux()
+	flag.Parse()
+	ext := filepath.Ext(*filePath)
 
-	// Build the MapHandler using the mux as the fallback
 	pathsToUrls := map[string]string{
 		"/urlshort-godoc": "https://godoc.org/github.com/gophercises/urlshort",
 		"/yaml-godoc":     "https://godoc.org/gopkg.in/yaml.v2",
 		"/my-github": "http://github.com/plastikov",
 	}
-	mapHandler := handler.MapHandler(pathsToUrls, mux)
+	handler.MapHandler(pathsToUrls, mux)
 
-	// Build the YAMLHandler using the mapHandler as the
-	// fallback
-
-	yamlHandler, err := handler.YAMLHandler([]byte(*yamlFlag), mapHandler)
-	if err != nil {
-		panic(err)
+	var hndlr http.Handler
+	var err error
+	if ext == ".yaml"{
+		hndlr, err = handler.YAMLHandler(printBytes(*filePath), mux)
+		if err != nil {
+			panic(err)
+		}
+	} else if ext == ".json"{
+		hndlr, err = handler.JSONHandler(printBytes(*filePath), mux)
+		if err != nil{
+			panic(err)
+		}
 	}
 	fmt.Println("Starting the server on :8080")
-	http.ListenAndServe(":8080", yamlHandler)
+	http.ListenAndServe(":8080", hndlr)
+}
+
+var (
+filePath = flag.String("File", "urls.yaml", "Pass into this flag a file holding data of url paths")
+)
+
+func printBytes(filepath string) []byte{
+	data, err := ioutil.ReadFile(filepath)
+	if err != nil{
+		panic(err)
+	}
+	return []byte(data)
 }
 
 func defaultMux() *http.ServeMux {
